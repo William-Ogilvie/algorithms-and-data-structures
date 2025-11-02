7f3dd64a3e095eaecd20198287af18c1aa56cfd3
William-Ogilvie <william.ogilvie@st-hughs.ox.ac.uk>
2025-11-02 16:20:04 +0000

    finished heap sort

--- sorting/heap_sort.py
+++ sorting/heap_sort.py
@@ -1,7 +1,7 @@
 """
 Heap Sort
 ------------
-Time Complexity = O(n log n) (Heapify takes log n need to do n times)
+Time Complexity = O(n log n) (Heapify takes log n need to do n times, I guess technically 2n bc have to build the inital heap too)
 Space = O(1) (in-place)
 Stable = No 
 
@@ -37,6 +37,8 @@ Steps:
 3. Heapify the remaining part again to restore the heap property
 """
 
+from sorting_benchmark import basic_benchmark, basic_test
+
 def heapify(arr, n, i):
     """
     The idea of heapifying is say some element doesn't satisfy the heap property, you then look at it's children 
@@ -56,7 +58,23 @@ def heapify(arr, n, i):
     if largest != i:
         arr[i], arr[largest] = arr[largest], arr[i]
         heapify(arr, n, largest)
-    
+
+def heap_sort(arr: list[int]):
+
+    # Create max heap out of arr
+    n = len(arr)
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, len(arr), i)
+
+    # Extract root of heap (largest elt) and put at end of array, then heapify the remaining part to restore the heap property
+    for i in range(n - 1, 0, - 1):
+        # Put root of heap at the end of the current heap
+        arr[0], arr[i] = arr[i], arr[0]
+
+        # Heapify the remaining list, now length i as we have removed the root from the heep
+        heapify(arr, i, 0)
+
+    return arr
     
 
 test_array = [3, 4, 10, 2, 5]
@@ -66,6 +84,15 @@ for i in range(len(test_array) // 2 - 1, -1, -1):
     heapify(test_array, len(test_array), i)
 print(test_array)
 
+# Basic test
+basic_test(heap_sort)
 
+# Basic benchmark
+n1 = int(1e3)
+n2 = int(1e4)
+basic_benchmark(n1, n2, heap_sort)
+print(
+    f"For O(n log n) we would expect a roughly 10*(1 + log 10 / log n1) = 13.33 fold" 
+    "increase in time, we get experimental around a 14 fold increase in time, which roughly matches")
 
 
